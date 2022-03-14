5550d1720374824693865bb3c031d5acf349f27d
francis-ohara <kofiohara@gmail.com>
2022-03-14 17:23:22 +0000

    Finished email-finder script.

--- email-finder/main.py
+++ email-finder/main.py
@@ -1,4 +1,15 @@
-"""script for printing out all emails in text files"""
-import re
+"""script for finding all email addresses in a text file if any"""
+from re import search
+from re import findall
 
-fh = open("")
+text_fh = open("text-files/email-text.txt")  # text file to be searched.
+text = text_fh.read()
+
+if search("\S+@\S+", text):  # check for presence of email address.
+    addresses = findall("\S+@\S+", text)  # generate list of all found email addresses.
+    print(f"{len(addresses)} emails found:")
+
+    for address in addresses:  # print all found email addresses.
+        print(address)
+else:
+    print("No emails found.")
